75796d69e60b8b181bb3ee817bbb3389bdafdbd3
Jeff Lewis <mail@jeffreylewis.me>
2019-11-11 19:23:16 -0600

    Add generic typings for object values

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,25 +4,25 @@ export type FieldName = string
 
 export type FieldPattern = FieldName | RegExp | FieldName[]
 
-export type UpdatesByName = {
-  [FieldName: string]: (value: any, allValues?: Object, prevValues?: Object) => any
+export type UpdatesByName<T = Object> = {
+  [FieldName: string]: (value: any, allValues?: T, prevValues?: T) => any
 }
 
-export type UpdatesForAll = (
+export type UpdatesForAll<T = Object> = (
   value: any,
   field: string,
-  allValues?: Object,
-  prevValues?: Object,
+  allValues?: T,
+  prevValues?: T
 ) => { [FieldName: string]: any }
 
-export type Updates = UpdatesByName | UpdatesForAll
+export type Updates<T = Object> = UpdatesByName<T> | UpdatesForAll<T>
 
-export type Calculation = {
-  field: FieldPattern,
-  updates: Updates,
-  isEqual?: (a: any, b: any) => boolean,
+export type Calculation<T = Object> = {
+  field: FieldPattern
+  updates: Updates<T>
+  isEqual?: (a: any, b: any) => boolean
 }
 
-export default function createDecorator(
-  ...calculations: Calculation[]
+export default function createDecorator<T = Object>(
+  ...calculations: Calculation<T>[]
 ): Decorator
